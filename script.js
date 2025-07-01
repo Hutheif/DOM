87ae3f1d57fe72b5c5deb1cd0e9d4919bcaa96b0
Hutheifa <ihudheifa87@gmail.com>
2025-07-01 13:00:31 +0300

    new files

---
+++ script.js
@@ -0,0 +1,19 @@
+//interface
+//web documents
+//interact with html
+
+//2. access elements through dom
+//querryselectorall
+//getelemetbyId
+//getelementsbyClassName
+
+//3.manipulate the content
+//textcontent
+//innerhtml
+//innertext
+
+//4.manipulate the structure
+
+//5. manipulate the style of elements
+
+//6.dealing with events
